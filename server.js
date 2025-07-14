4f1d44d819731bd781cc78db4158aea06c21abd3
Ramanakumar05 <ramanakumarr.05@gmail.com>
2025-07-14 11:59:28 +0530

    c6

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 Hello from Dockerized Node.js App with Jenkins!');
+  res.send(' Hello from Dockerized Node.js App with Jenkins This is to show1!');
 });
 
 app.listen(port, () => {
