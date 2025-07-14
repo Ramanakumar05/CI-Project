4038725560dc921fefb1ad897b9a1f0c07431051
Ramanakumar05 <ramanakumarr.05@gmail.com>
2025-07-14 18:17:26 +0530

    c7

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+
+app.get('/pass',(req,res)=>
+{
+  res.send('password ramana')
+})
 app.get('/', (req, res) => {
   res.send(' Hello from Dockerized Node.js App with Jenkins This is to show2!');
 });
